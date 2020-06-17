dc0c2c7c4ffbc6a3a1e3d1868d6ba78806c569c7
panosmania <panostser@yahoo.gr>
2020-06-17 18:45:51 +0300

    try to fix a bug in sign up form

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -63,7 +63,7 @@ const sendErrorProd = (err, req, res) => {
 
     // 2) Send generic message
     return res.status(500).json({
-      status: error,
+      status: 'error',
       message: 'Something went very wrong!',
     });
   }
