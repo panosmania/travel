9a91d23c228a9f3f1510dbc037f6ad48164ce056
panosmania <panostser@yahoo.gr>
2020-06-17 17:37:28 +0300

    try to fix a bug in sign up form

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,52 +1,29 @@
 /* eslint-disable */
+
 import axios from 'axios';
 import { showAlert } from './alerts';
 
-export const signup = async (
-  name,
-  emailSignup,
-  passwordSignup,
-  passwordConfirm
-) => {
+export const signup = async (name, email, password, passwordConfirm) => {
+  console.log(signup);
   try {
-    console.log('prin');
-
     const res = await axios({
       method: 'POST',
-      //url: 'http://127.0.0.1:3000/api/v1/users/signup',
       url: '/api/v1/users/signup',
       data: {
-        name: name,
-        email: emailSignup,
-        password: passwordSignup,
-        passwordConfirm: passwordConfirm,
+        name,
+        email,
+        password,
+        passwordConfirm,
       },
     });
-    console.log('μπηκαν', res);
+
     if (res.data.status === 'success') {
-      showAlert('success', 'Account successfully created!');
+      showAlert('success', 'Account created successfully');
       window.setTimeout(() => {
-        location.replace('/');
+        location.assign('/');
       }, 1500);
     }
-
-    console.log('μετα', res);
   } catch (err) {
-    console.log(err.response);
-    //showAlert('error', err.response.data.message);
-    //`Invalid input data. ${err.response.data.message.split(':')[1]}!`
-    showAlert('error', `${err.response.data.message}!`);
+    showAlert('error', err.response.data.message);
   }
 };
-
-// document.querySelector('.formsignup').addEventListener('submit', (e) => {
-//   e.preventDefault();
-
-//   //take email and password value that the user put in from the browser
-//   const name = document.getElementById('name').value;
-//   const email = document.getElementById('email').value;
-//   const password = document.getElementById('password').value;
-//   const passwordConfirm = document.getElementById('passwordConfirm').value;
-
-//   signup(name, email, password, passwordConfirm);
-// });
